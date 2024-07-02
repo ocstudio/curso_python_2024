5fc26d8dcef7e0615318c8c735354f5e5caa61f8
Orlando cienfuegos <ayo100fuegos@gmail.com>
2024-07-02 16:21:22 -0500

    Comentario de la clase y conceptos basicos

--- 03_clase.py
+++ 03_clase.py
@@ -1,3 +1,9 @@
+'''Comprender los conceptos de semántica y sintaxis es crucial en programación.
+La semántica da sentido al código, asegurando consistencia y coherencia en las operaciones,
+mientras que la sintaxis dicta cómo escribir correctamente el código, siguiendo reglas
+específicas.'''
+
+
 saludo1 = "hola"
 saludo2 = "hi"
 nombre = "Orlando"
