7f87aa0c95440ed11f85851be2ee69fc4b560445
ocstudio Fedora <ayo100fuegos@mail.com>
2024-10-16 12:38:20 -0500

    nuevo inicio de curso

--- 07_clase.py
+++
@@ -1,8 +0,0 @@
-#Interaccion con en usuario funcion input 
-
-name = input("ingrese su nombre : " )
-age = input("ingrese su edad : " )
-print(name)
-print(age)
-
-
--- suma_matrix.py
+++
@@ -1,8 +0,0 @@
-matrix_1 =[[1,2,3],
-	[4,5,6,]]
-
-matrix_2 =[[7,8,9],
-	[1,2,3]]
-
-print ("suma:", matrix_1 + matrix_2) 
-
